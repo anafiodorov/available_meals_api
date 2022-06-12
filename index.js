156c12dd38590780eb0035f5de63510e5c223c9a
ciupaciups <fiodorovana@yahoo.ro>
2022-06-12 12:22:54 +0300

    update index.js last

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ app.use(function (req, res, next) {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader(
     'Access-Control-Allow-Headers',
